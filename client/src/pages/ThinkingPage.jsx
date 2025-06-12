75a6a47072c55f74ba0211bbdb6d1abb3a4d7bde
arpitshukla9 <arpitshukla0901@gmail.com>
2025-06-12 17:08:23 +0530

    fix: remove navbar error

--- client/src/pages/ThinkingPage.jsx
+++ client/src/pages/ThinkingPage.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import Navbar from '../components/Navbar.jsx';
 import Think from '../components/Thinking.jsx';
 
 const ThinkingPage = () => {
   return (
     <div className="min-h-screen">
-      <Navbar />
       <div className="py-8">
         <Think />
       </div>
