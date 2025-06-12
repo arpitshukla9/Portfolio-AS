1ac38a7374d88557573a8ae7adce7a345a67e878
arpitshukla9 <arpitshukla0901@gmail.com>
2025-06-12 20:01:07 +0530

    🚀 Backend working with deployed API on Render

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -110,7 +110,7 @@ const ContactUs = () => {
   };
 
   const submitToBackend = async (data) => {
-   const response = await fetch('https://arpitshukla-portfolio.onrender.com/api/contact', {
+   const response = await fetch('https://portfolio-as.onrender.com/api/contact', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
--- server/src/server.js
+++ server/src/server.js
@@ -9,7 +9,11 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({
+  origin: ['https://www.arpitshuklatech.com', 'http://localhost:5173'],
+  credentials: true
+}));
+
 app.use(express.json());
 
 app.use('/api/contact', contactRoutes);
